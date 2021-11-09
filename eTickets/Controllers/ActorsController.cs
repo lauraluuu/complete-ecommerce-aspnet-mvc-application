eb6b8da1ef10c8f7c8cdfe10f11a49719d21c94c
Laura Lu <53279344+lauraluuu@users.noreply.github.com>
2021-11-09 14:44:52 +1300

    edit actor details

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -22,6 +22,7 @@ namespace eTickets.Controllers
             var data = await _service.GetAllAsync();
             return View(data);
         }
+
         //Get: Actors/Create
         public IActionResult Create()
         {
@@ -44,8 +45,27 @@ namespace eTickets.Controllers
         {
             var actorDetails = await _service.GetByIdAsync(id);
 
-            if (actorDetails == null) return View("Empty");
+            if (actorDetails == null) return View("NotFound");
             return View(actorDetails);
+        }
+
+        //Get: Actors/Edit
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, FullName, ProfilePictureURL, Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _service.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
